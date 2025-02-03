16efe60d831afb40a74803d5ae0e5da720318610
Anan Li <li0047@hz.nl>
2025-02-03 18:08:23 +0100

    adjust add button position

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,29 +6,29 @@
 <footer class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 shadow-md">
     <div class="flex justify-around items-center h-16">
         <!-- Home -->
-        <button class="flex flex-col items-center text-gray-700 hover:text-black">
+        <button class="flex flex-col items-center text-gray-700 hover:text-black" on:click={ () => goto('/home') }>
             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M3 13h1v7c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2v-7h1a1 1 0 0 0 .707-1.707l-9-9a.999.999 0 0 0-1.414 0l-9 9A1 1 0 0 0 3 13zm9-8.586 6 6V15l.001 5H6v-9.585l6-6.001z"></path>
                 <path d="M12 17c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z"></path>
             </svg>
-            <span class="text-xs" on:click={ () => goto('/') }>Home</span>
+            <span class="text-xs">Home</span>
         </button>
 
         <hr class="border-gray-300 h-6">
 
         <!-- Calendar -->
-        <button class="flex flex-col items-center text-gray-700 hover:text-black">
+        <button class="flex flex-col items-center text-gray-700 hover:text-black" on:click={ () => goto('/calendar') }>
             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
                 <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
             </svg>
-            <span class="text-xs" on:click={ () => goto('/calendar') }>Calendar</span>
+            <span class="text-xs">Calendar</span>
         </button>
 
         <hr class="border-gray-300 h-6">
 
         <!-- More -->
-        <button class="flex flex-col items-center text-gray-700 hover:text-black">
+        <button class="flex flex-col items-center text-gray-700 hover:text-black" on:click={ () => goto('/more') }>
             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path d="m2.344 15.271 2 3.46a1 1 0 0 0 1.366.365l1.396-.806c.58.457 1.221.832 1.895 1.112V21a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-1.598a8.094 8.094 0 0 0 1.895-1.112l1.396.806c.477.275 1.091.11 1.366-.365l2-3.46a1.004 1.004 0 0 0-.365-1.366l-1.372-.793a7.683 7.683 0 0 0-.002-2.224l1.372-.793c.476-.275.641-.89.365-1.366l-2-3.46a1 1 0 0 0-1.366-.365l-1.396.806A8.034 8.034 0 0 0 15 4.598V3a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1.598A8.094 8.094 0 0 0 7.105 5.71L5.71 4.904a.999.999 0 0 0-1.366.365l-2 3.46a1.004 1.004 0 0 0 .365 1.366l1.372.793a7.683 7.683 0 0 0 0 2.224l-1.372.793c-.476.275-.641.89-.365 1.366zM12 8c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4z"></path>
             </svg>
